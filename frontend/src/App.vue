fa45316d9a907eebd7dcf4afba1dcbe4cb797d4e
hfxz <xuan__zai@outlook.com>
2025-12-23 17:19:19 +0800

    style: supprt safe-area for ios

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,8 +3,8 @@ import AppTabbar from '@/components/layouts/AppTabbar.vue'
 </script>
 
 <template>
-  <main class="h-screen flex flex-col">
-    <div class="flex-1 overflow-auto">
+  <main class="h-screen flex flex-col bg-gray-50">
+    <div class="flex-1 overflow-auto p-4">
       <router-view />
     </div>
     <app-tabbar />
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,10 +1,5 @@
-<script lang="ts" setup>
-const handleClick = () => {
-  showToast('You clicked the Vant Button!')
-}
-</script>
+<script lang="ts" setup></script>
 
 <template>
-  <van-button type="primary" @click="handleClick">Vant Button</van-button>
   <h1 class="text-3xl font-bold text-blue-600">Hello Tailwind</h1>
 </template>
