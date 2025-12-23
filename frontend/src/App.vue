0a47be8188dc04c8480539f01fa66d15c1d08a53
hfxz <xuan__zai@outlook.com>
2025-12-23 15:42:38 +0800

    chore: add vant UI library
    - also add unplugin for auto import

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,4 +1,8 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const handleClick = () => {
+  showToast('You clicked the Vant Button!')
+}
+</script>
 
 <template>
   <h1>You did it!</h1>
@@ -6,6 +10,7 @@
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
+  <van-button type="primary" @click="handleClick">Vant Button</van-button>
 </template>
 
 <style scoped></style>
