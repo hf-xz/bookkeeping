ecf19ed09e1f68b2bcac9d3b953641f5f461da8b
hfxz <xuan__zai@outlook.com>
2025-12-24 10:07:30 +0800

    chore: add cors to backend

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers import metrics, transactions
 
 app = FastAPI()
 
 app.include_router(metrics.router)
 app.include_router(transactions.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
