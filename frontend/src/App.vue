371148105554aa00e3c79e48f709d79081991cda
hfxz <xuan__zai@outlook.com>
2025-12-30 16:42:53 +0800

    fix: 修复 ios safari 滚动问题
    - 用 h-dvh 替换 h-screen 以获得更好的移动端支持

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,10 +5,9 @@ const tabbarHeight = computed(() => tabbarRef.value?.$el.offsetHeight || 0)
 </script>
 
 <template>
-  <main class="h-screen flex flex-col bg-gray-100">
+  <main class="h-dvh bg-gray-100">
     <!-- TODO title bar -->
-    <!-- TODO content height -->
-    <div class="flex-1 overflow-auto p-4" :style="{ marginBottom: tabbarHeight + 'px' }">
+    <div class="h-full overflow-auto p-4" :style="{ marginBottom: tabbarHeight + 'px' }">
       <router-view v-slot="{ Component }">
         <keep-alive>
           <component :is="Component" />
