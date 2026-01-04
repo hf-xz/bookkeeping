5b33263fae0700ab8129e14a91face6d87688a4a
hfxz <xuan__zai@outlook.com>
2026-01-04 13:40:53 +0800

    feat: 前后选择日期并更新

--- frontend/src/views/RecordView/MyDatePicker.vue
+++ frontend/src/views/RecordView/MyDatePicker.vue
@@ -1,13 +1,34 @@
 <script lang="ts" setup>
 const props = defineProps<{
-  disabled?: boolean
+  selectDate: string
+  disabled: boolean
 }>()
+
+const date = computed({
+  get: () => props.selectDate,
+  set: (val: string) => {
+    emit('update:selectDate', val)
+  },
+})
+
+const emit = defineEmits<{
+  (e: 'update:selectDate', val: string): void
+}>()
+
+const selectYesterday = () => {
+  date.value = addDate(date.value, -1)
+}
+const selectTommorow = () => {
+  date.value = addDate(date.value, 1)
+}
+
+const today = getToday()
 </script>
 
 <template>
-  <div class="w-full grid grid-cols-[4rem_auto_4rem]">
-    <van-button plain :disabled icon="arrow-left" />
-    <div class="text-center">date</div>
-    <van-button plain :disabled icon="arrow" />
+  <div class="w-full grid grid-cols-[4rem_auto_4rem] items-center">
+    <van-button plain :disabled icon="arrow-left" @click="selectYesterday" />
+    <span class="text-md text-center font-mono">{{ date }}</span>
+    <van-button plain :disabled="disabled || date == today" icon="arrow" @click="selectTommorow" />
   </div>
 </template>
--- frontend/src/views/RecordView/index.vue
+++ frontend/src/views/RecordView/index.vue
@@ -8,12 +8,11 @@ import MyDatePicker from './MyDatePicker.vue'
 const records = ref<Record<string, Array<TransactionWithMetricName>>>({})
 
 // 更新记录缓存
-const updateRecords = (txns: TransactionWithMetricName[]) => {
-  for (const txn of txns) {
-    if (!records.value[txn.record_date]) {
-      records.value[txn.record_date] = []
-    }
-    records.value[txn.record_date]!.push(txn)
+const updateRecords = (txns: TransactionWithMetricName[], startDate: string, endDate: string) => {
+  let day = startDate
+  while (day <= endDate) {
+    records.value[day] = txns.filter((t) => t.record_date === day)
+    day = addDate(day, 1)
   }
 }
 
@@ -27,20 +26,32 @@ const formEditing = ref(false)
 // 更新指定日期的记录
 const updateDateRecords = async (date: string) => {
   const res = await readOnedayTransactions(date)
-  updateRecords(res)
+  updateRecords(res, date, date)
 }
 
 onMounted(async () => {
   // 初始加载最近一周的记录
   const startDate = addDate(today, -6)
   const res = await readTransactions(startDate, today)
-  updateRecords(res)
+  updateRecords(res, startDate, today)
+
+  // 监听日期变化，加载对应日期的记录
+  // HACK 每次多加载一些日期，避免频繁请求
+  watch(
+    () => currentDate.value,
+    (newDate) => {
+      // 缓存没有才加载
+      if (!records.value[newDate]) {
+        updateDateRecords(newDate)
+      }
+    },
+  )
 })
 </script>
 
 <template>
   <main class="h-full flex flex-col p-4 gap-4">
-    <MyDatePicker :disabled="formEditing" />
+    <MyDatePicker v-model:select-date="currentDate" :disabled="formEditing" />
     <DayRecordForm
       class="flex-1"
       v-model:formEditing="formEditing"
